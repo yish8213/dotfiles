12da9f0fa260aeffc268b6a6aaf4f9a4603c5bb9
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-03-11 22:55:49 +0900

    Add some Atom Editor's plugins

--- editors/install/install.sh
+++ editors/install/install.sh
@@ -2,3 +2,14 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 brew bundle --file="$THIS_DIR/Brewfile"
+
+
+# Install Atom plugins
+## https://atom.io/packages/sync-settings
+apm install sync-settings
+
+## https://atom.io/packages/auto-update-packages
+apm install auto-update-packages
+
+## https://atom.io/packages/file-icons
+apm install file-icons
