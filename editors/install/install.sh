44d7cd7fa5105ec6b605bb22b4cfcb2e580fb7c6
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-03-13 02:28:17 +0900

    Install Atom Editor's plugins just once.

--- editors/install/install.sh
+++ editors/install/install.sh
@@ -6,10 +6,19 @@ brew bundle --file="$THIS_DIR/Brewfile"
 
 # Install Atom plugins
 ## https://atom.io/packages/sync-settings
-apm install sync-settings
+if test ! -d "$HOME/.atom/packages/sync-settings"
+then
+  apm install sync-settings
+fi
 
 ## https://atom.io/packages/auto-update-packages
-apm install auto-update-packages
+if test ! -d "$HOME/.atom/packages/auto-update-packages"
+then
+  apm install auto-update-packages
+fi
 
 ## https://atom.io/packages/file-icons
-apm install file-icons
+if test ! -d "$HOME/.atom/packages/file-icons"
+then
+  apm install file-icons
+fi
