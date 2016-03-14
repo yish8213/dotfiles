a5fe61972e69035b0ff55daaf193b39fc1b2fc49
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-03-15 00:17:23 +0900

    Run `chsh` when it is necessary to.

--- fish/install/install.sh
+++ fish/install/install.sh
@@ -3,18 +3,19 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 THIS_SHELL='fish'
+THIS_SHELL_PATH="/usr/local/bin/$THIS_SHELL"
 
-if test ! -f "/usr/local/bin/$THIS_SHELL"
+if test ! -f "$THIS_SHELL_PATH"
 then
   # Install Fish shell.
   brew bundle --file="$THIS_DIR/Brewfile"
 
   # Add the new shell to the list of allowed shells
-  sudo bash -c "echo /usr/local/bin/$THIS_SHELL >> /etc/shells"
+  sudo bash -c "echo $THIS_SHELL_PATH >> /etc/shells"
+fi
 
-  # Change to the new shell
-  if [ "$LOGIN_SHELL" == "$THIS_SHELL" ]
-  then
-    chsh -s "/usr/local/bin/$LOGIN_SHELL"
-  fi
+# Change to the new shell
+if [ "$LOGIN_SHELL" == "$THIS_SHELL" ] && [ "$THIS_SHELL_PATH" == "`finger -l $USER | awk '/Shell/' | awk '{ print $4 }'`" ]
+then
+  chsh -s "$THIS_SHELL_PATH"
 fi
