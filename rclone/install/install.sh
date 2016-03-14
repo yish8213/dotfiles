dbd5f2c2133f14777ffcd8e5b04b9d5eb1b8c9d3
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-03-14 23:04:30 +0900

    Add a new module `rclone`, which can be used to upload files to Amazon Cloud Drive.

---
+++ rclone/install/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+THIS_DIR=$(cd "$(dirname "$0")"; pwd)
+
+brew bundle --file="$THIS_DIR/Brewfile"
+
+# http://rclone.org/install/
+export GOPATH=$HOME/.rclone
+
+if test ! -f "$GOPATH/bin/rclone"
+then
+  mkdir -p "$GOPATH/src" && go get github.com/ncw/rclone
+fi
