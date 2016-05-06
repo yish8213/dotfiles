91df549ca9b642e62820ca7219bef07e38025a7d
Choi Jaehoon <plaintext@andromedarabbit.net>
2016-05-07 00:45:52 +0900

    `chmod 600` on ssh key files.

--- ssh/install/install.sh
+++ ssh/install/install.sh
@@ -2,3 +2,6 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 brew bundle --file="$THIS_DIR/Brewfile"
+
+
+find "$HOME/.ssh" \( -name *pem -o -name id_dsa -o -name id_rsa \) -exec sh -c 'chmod 600 "$0"' {} \;
